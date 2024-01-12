392beedfd14e6c97d1038a793633875f58c896b1
Z0F <28806618+0xZ0F@users.noreply.github.com>
2024-01-12 18:26:56 -0500

    formatting

--- Client/Client.py
+++ Client/Client.py
@@ -4,36 +4,41 @@ import socket
 import argparse
 import struct
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('ip', type=str, metavar="ip", help="IP address of server")
-    parser.add_argument('port', type=int, metavar="port", help="Port of server")
+    parser.add_argument("ip", type=str, metavar="ip", help="IP address of server")
+    parser.add_argument("port", type=int, metavar="port", help="Port of server")
     args = parser.parse_args()
-        
+
     print("Tunnel client started.")
-    
+
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-           sock.connect((args.ip, args.port))
-           while True:
-               data = input("> ")
+            sock.connect((args.ip, args.port))
+            while True:
+                data = input("> ")
 
-               size = len(data)
-               op = 0
-               index = 0
-               data = data.encode()
+                size = len(data)
+                op = 0
+                index = 0
+                data = data.encode()
 
-               buf = struct.pack(f'!LHH{len(data)}s', size, op, index, data)
-               print(f"Send: {buf}")
+                buf = struct.pack(f"!LHH{len(data)}s", size, op, index, data)
+                print(f"Send: {buf}")
 
-               sock.send(buf)
-               
-               resp = sock.recv(1024)
-               
-               print(f"Recv: {resp}")
+                print(len(buf))
+                print(buf.hex())
+
+                sock.send(buf)
+
+                resp = sock.recv(1024)
+
+                print(f"Recv: {resp}")
 
     except socket.error as err:
         print(err.strerror)
+
 
 if __name__ == "__main__":
     try:
